787972d722f03fbc9be8b993b7b46446a7469a64
linronghui <frankie.lin@yulong.com>
2019-04-03 11:39:06 +0800

    add note

--- cppPractice/T2_revert_byte/main.cpp
+++ cppPractice/T2_revert_byte/main.cpp
@@ -18,13 +18,67 @@ unsigned char revertByte(unsigned char input) {
     return output;
 }
 
+void test()
+{
+    int A[10] = {0};
+
+    A[11] = 0;
+}
+
+
+void revint8(unsigned char input)
+{
+    unsigned char output = 0;
+
+    printf("input is: %x\n", input);
+    for(int i=0; i<8; i++)
+    {
+        if(input & (1<<i))
+        {
+            cout << "find bit 1, i=" << i << endl;
+            output |= (1<<(7-i));
+        }
+    }
+    printf("onput is: %x\n", output);
+}
+
+template <class T>
+T revT(T t)
+{
+    T tOut = 0;
+    printf("template input is: %x\n", t);
+
+    for(int i=0; i < sizeof(T)*8; i++)
+    {
+        if(t & (1<<i))
+          tOut |= (1<<(sizeof(T)*8 -1 - i));
+    }
+    printf("template onput is: %x\n", tOut);
+    return tOut;
+}
+
 int main()
 {
+    cout << (1 << 0) << endl;
+    /*
     unsigned char test = 58;
     unsigned char result = revertByte(test);
 
     printf("result is %d\n", result);
     cout << result << endl;
+*/
+    //int i = 4/2*3;
+    //cout << i << endl;
+    //test();
+    //
+    unsigned char in = 0x53;
+    revint8(in);
+
+    revT(in);
+
+    unsigned int in2 = 0x8A;
+    revT(in2);
+
 
     return 0;
 }
