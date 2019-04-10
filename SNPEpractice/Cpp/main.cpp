9fc72cdc713a6316aec3434f25c1946f5a859af7
linronghui <frankie.lin@yulong.com>
2019-04-10 16:12:30 +0800

    add open container file

--- SNPEpractice/Cpp/main.cpp
+++ SNPEpractice/Cpp/main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
+#include <fstream>
 #include "SNPE/SNPE.hpp"
 #include "SNPE/SNPEFactory.hpp"
 #include "DlSystem/DlVersion.hpp"
 #include "DlSystem/DlEnums.hpp"
 #include "DlSystem/String.hpp"
-
+#include "DlContainer/IDlContainer.hpp"
 
 
 using namespace std;
 
 int main()
 {
+    string dlc = "/sdcard/test.dlc";
+    string testPic = "/sdcard/test.jpg";
+
     cout << "hello" << endl;
-    static zdl::DlSystem::Runtime_t runtime = zdl::DlSystem::Runtime_t::CPU;
+    static zdl::DlSystem::Runtime_t runtime = zdl::DlSystem::Runtime_t::GPU;
     static zdl::DlSystem::Version_t Version = zdl::SNPE::SNPEFactory::getLibraryVersion();
     cout << "SNPE Version: " << Version.asString().c_str() << endl; //Print Version number
+
+    if(zdl::SNPE::SNPEFactory::isRuntimeAvailable(runtime))
+    {
+        cout << "target support GPU" << endl;
+    }
+    else
+    {
+        runtime = zdl::DlSystem::Runtime_t::CPU;
+        cout << "target not support GPU" << endl;
+        cout << "fall back to CPU" << endl;
+    }
+
+    ifstream dlcFile(dlc);
+    ifstream inputPic(testPic);
+    if(!dlcFile || !inputPic) {
+        cout << "no dlc file or input picture" << endl;
+        return -1;
+    }
+
+    unique_ptr<zdl::DlContainer::IDlContainer> container =
+        zdl::DlContainer::IDlContainer::open(zdl::DlSystem::String(dlc.c_str()));
+    if (container == nullptr) {
+        cout << "can not open container file!" << endl;
+        return -1;
+    }
+
+
 
     cout << "hello222" << endl;
     return 0;
